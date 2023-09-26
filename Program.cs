1117b50a885b19cad1c360642321b8d56aac1bb8
Heba Derawi <heba.derawi95@hotmail.com>
2023-09-26 14:10:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Labb4___Felsökning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //case 1
+            int number = 2;
+
+            if (number > 3)
+            {
+                Console.WriteLine("Talet är större än tre"); // detta ";" tecken saknades.
+            }
+
+            else if(number < 3) //la till mellanslag mellan else och if.
+{
+                Console.WriteLine("Talet är mindre än tre");
+            }
+        }
+    }
+}
